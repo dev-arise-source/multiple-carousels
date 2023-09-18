a11428352a6a4b190831c003de3bbdc304cbc851
Alex Chika <mcluckey1@gmail.com>
2023-09-18 19:14:21 +0100

    added icon to home carousel card

--- src/pages/1-image-slideshow/index.tsx
+++ src/pages/1-image-slideshow/index.tsx
@@ -3,7 +3,7 @@ import ImageSlideShow from "./components/ImageSlideShow";
 function ImageSlideShow1() {
   return (
     <section className="w-full max-w-3xl mx-auto">
-      <ImageSlideShow autoplay={false} interval={5} />
+      <ImageSlideShow autoplay={true} interval={5} />
     </section>
   );
 }
--- src/pages/home/components/CarouselsCard.tsx
+++ src/pages/home/components/CarouselsCard.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import { Carousel } from "../asset"; // TS type
 import youtubeLogo from "../asset/youtube-logo.png"; // Youtube logo
+import InfoIcon from "../asset/infoIcon";
+import EyeIcon from "../asset/EyeIcon";
 
 function CarouselsCard({ carousel }: { carousel: Carousel }) {
   const { img, title, url, youtubeUrl } = carousel;
@@ -15,7 +17,7 @@ function CarouselsCard({ carousel }: { carousel: Carousel }) {
         href={youtubeUrl}
         target="_blank"
         rel="noopener noreferrer"
-        className="absolute flex justify-center items-center w-0 h-0 opacity-0 transition-all duration-500 ease-in-out bg-slate-900/50 rounded-none group-hover:opacity-100 group-hover:w-20 group-hover:h-20 group-hover:rounded-full"
+        className="absolute flex justify-center items-center w-0 h-0 opacity-0 transition-all duration-500 ease-in-out bg-slate-900/50 rounded-full group-hover:opacity-100 group-hover:w-20 group-hover:h-20"
       >
         <span className="h-12 w-12 rounded-full overflow-hidden">
           <img
@@ -32,32 +34,12 @@ function CarouselsCard({ carousel }: { carousel: Carousel }) {
           // onClick={}
           className="flex items-center gap-1 mr-3"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            className="text-sm"
-            viewBox="0 0 16 16"
-          >
-            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-            <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
-          </svg>
-
+          <InfoIcon />
           <span className="italic text-xs">Info...</span>
         </button>
 
         <Link className="flex items-center gap-1 text-cyan-500" to={url}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            viewBox="0 0 16 16"
-          >
-            <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
-            <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
-          </svg>
+          <EyeIcon />
           <span className="italic text-xs">Demo</span>
         </Link>
       </div>
